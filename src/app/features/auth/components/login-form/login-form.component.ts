0d058c54e5529b68bdd1aef070752091e2d55acc
Alexandre Lamberty <alexandrelamberty@gmail.com>
2023-03-21 09:19:36 +0100

    create subscription

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -8,6 +8,7 @@ import {
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { AuthResult } from '../../models/auth-result';
+import { Subscription } from 'rxjs';
 
 /**
  * Reactive form with FormBuilder service.
@@ -17,7 +18,9 @@ import { AuthResult } from '../../models/auth-result';
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
 })
-export class LoginFormComponent implements OnInit {
+export class LoginFormComponent implements OnInit, OnDestroy {
+  loginSubscription: Subscription = new Subscription();
+
   loginForm = this.formBuilder.group({
     email: new FormControl('', [
       Validators.required,
@@ -40,16 +43,23 @@ export class LoginFormComponent implements OnInit {
 
   ngOnInit() {}
 
+  ngOnDestroy(): void {
+    this.loginSubscription.unsubscribe();
+  }
+
   onSubmit(): void {
     const user = this.loginForm.value;
-    this.authService.login(user).subscribe({
+    this.loginSubscription = this.authService.login(user).subscribe({
       next: (res: AuthResult) => {
         // FIXME: move to service
         localStorage.setItem('token', res.result.token);
         localStorage.setItem('userId', res.result.user.id.toString());
         localStorage.setItem('userRole', res.result.user.role);
       },
-      error: (err) => {},
+      error: (error) => {
+        console.log('LoginForm Error', error);
+        // FIXME: notification service ?
+      },
       complete: () => {
         this.loginForm.reset();
         this.router.navigate(['/']);
